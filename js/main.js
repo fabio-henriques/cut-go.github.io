32ff7e2ccccb93092c4471f3177d866f3396aa2e
fabio-henriques <fabio.ghenriques@gmail.com>
2017-06-13 01:00:04 +0100

    adding my files to my repository

---
+++ js/main.js
@@ -0,0 +1,219 @@
+$(document).ready(function() {
+
+	$( '#example3' ).sliderPro({
+		width: 960,
+		height: 500,
+		fade: true,
+		arrows: true,
+		buttons: false,
+		fullScreen: true,
+		shuffle: true,
+		smallSize: 500,
+		mediumSize: 1000,
+		largeSize: 3000,
+		thumbnailArrows: true,
+		autoplay: false
+	});
+
+/*-------------------------------language-menu-----------------------------*/
+
+	$('.language').on('click', '.language__main', function(e){
+
+		e.preventDefault();
+
+		$('.language__main').addClass('language__menu--none');
+
+		$('.language__main--active').removeClass('language__menu--none');
+
+		$('.language__menu').removeClass('language__menu--none');
+
+	});
+
+	$('.language').on('click', '.language__main--active', function(e){
+
+		e.preventDefault();
+
+		$('.language__main').removeClass('language__menu--none');
+
+		$('.language__main--active').addClass('language__menu--none');
+
+		$('.language__menu').addClass('language__menu--none');
+		
+	});
+
+/*-----------------------------scroll-top-------------------------------------*/
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+/*-----------------------------search-responsive-------------------------------------*/
+	
+	$('.menu__toggle').on('click', '.menu__toggle__link', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		//$('.nav__list').slideToggle()
+		$('.menu').toggleClass('menu__open');
+		$('.menu__toggle__link').toggleClass('menu__toggle__link--open');
+	}); 
+
+/*---------------------------------modal------------------------------------------*/
+
+	var formModal = $('.cd-user-modal'),
+		formLogin = formModal.find('#cd-login'),
+		formSignup = formModal.find('#cd-signup'),
+		formForgotPassword = formModal.find('#cd-reset-password'),
+		formModalTab = $('.cd-switcher'),
+		tabLogin = formModalTab.children('li').eq(0).children('a'),
+		tabSignup = formModalTab.children('li').eq(1).children('a'),
+		forgotPasswordLink = formLogin.find('.cd-form-bottom-message a'),
+		backToLoginLink = formForgotPassword.find('.cd-form-bottom-message a'),
+		mainNav = $('.menu');
+
+	//open modal
+	mainNav.on('click', function(event){
+		$(event.target).is(mainNav) && mainNav.children('ul').toggleClass('is-visible');
+	});
+
+	//open sign-up form
+	mainNav.on('click', '.cd-signup', signup_selected);
+	//open login-form form
+	mainNav.on('click', '.cd-signin', login_selected);
+
+	//close modal
+	formModal.on('click', function(event){
+		if( $(event.target).is(formModal) || $(event.target).is('.cd-close-form') ) {
+			formModal.removeClass('is-visible');
+		}	
+	});
+	//close modal when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		formModal.removeClass('is-visible');
+	    }
+    });
+
+	//switch from a tab to another
+	formModalTab.on('click', function(event) {
+		event.preventDefault();
+		( $(event.target).is( tabLogin ) ) ? login_selected() : signup_selected();
+	});
+
+	//hide or show password
+	$('.hide-password').on('click', function(){
+		var togglePass= $(this),
+			passwordField = togglePass.prev('input');
+		
+		( 'password' == passwordField.attr('type') ) ? passwordField.attr('type', 'text') : passwordField.attr('type', 'password');
+		( 'Esconder' == togglePass.text() ) ? togglePass.text('Mostrar') : togglePass.text('Esconder');
+		//focus and move cursor to the end of input field
+		passwordField.putCursorAtEnd();
+	});
+
+	//show forgot-password form 
+	forgotPasswordLink.on('click', function(event){
+		event.preventDefault();
+		forgot_password_selected();
+	});
+
+	//back to login from the forgot-password form
+	backToLoginLink.on('click', function(event){
+		event.preventDefault();
+		login_selected();
+	});
+
+	function login_selected(){
+		mainNav.children('ul').removeClass('is-visible');
+		formModal.addClass('is-visible');
+		formLogin.addClass('is-selected');
+		formSignup.removeClass('is-selected');
+		formForgotPassword.removeClass('is-selected');
+		tabLogin.addClass('selected');
+		tabSignup.removeClass('selected');
+	}
+
+	function signup_selected(){
+		mainNav.children('ul').removeClass('is-visible');
+		formModal.addClass('is-visible');
+		formLogin.removeClass('is-selected');
+		formSignup.addClass('is-selected');
+		formForgotPassword.removeClass('is-selected');
+		tabLogin.removeClass('selected');
+		tabSignup.addClass('selected');
+	}
+
+	function forgot_password_selected(){
+		formLogin.removeClass('is-selected');
+		formSignup.removeClass('is-selected');
+		formForgotPassword.addClass('is-selected');
+	}
+
+	//IE9 placeholder fallback
+	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+		  	}
+		}).blur(function() {
+		 	var input = $(this);
+		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+		  	}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+		  	$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+			 		input.val('');
+				}
+		  	})
+		});
+	}
+
+});
+
+
+/*-------------------------------search-bar----------------------------------------*/
+
+function Mudarestado(el) {
+        var display = document.getElementById(el).style.display;
+        if(display == "block")
+            document.getElementById(el).style.display = 'none';
+        else
+            document.getElementById(el).style.display = 'block';
+    }
+
+/*---------------------------------modal------------------------------------------*/
+
+//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+$.fn.putCursorAtEnd = function() {
+	return this.each(function() {
+    	// If this function exists...
+    	if (this.setSelectionRange) {
+      		// ... then use it (Doesn't work in IE)
+      		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+      		var len = $(this).val().length * 2;
+      		this.focus();
+      		this.setSelectionRange(len, len);
+    	} else {
+    		// ... otherwise replace the contents with itself
+    		// (Doesn't work in Google Chrome)
+      		$(this).val($(this).val());
+    	}
+	});
+};
+
+
+
+
